f6e99bfa8f121ed1563f29af054ad530bf560808
TianKaiJr <user.gopikrishnavinod@gmail.com>
2024-04-10 15:43:43 +0530

    firebased connected

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -3,8 +3,14 @@ import 'package:flutter_application_2/app_pages/functionality_page.dart';
 import 'package:flutter_application_2/app_pages/login_page.dart';
 import 'package:flutter_application_2/app_pages/regist_page.dart';
 import 'package:flutter_application_2/components/my_container.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -16,11 +22,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        'x':(context) => RegistrationPage(),
-        'y':(context) =>  LoginPage(),
-        'z':(context) => FunPage(),
+        'x': (context) => RegistrationPage(),
+        'y': (context) => LoginPage(),
+        'z': (context) => FunPage(),
       },
-      home:LoginPage(), 
+      home: LoginPage(),
     );
   }
 }
