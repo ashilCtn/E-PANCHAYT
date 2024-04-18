1b86b0255a96bb96b350883e2b4d28de8720d0f9
TianKaiJr <user.gopikrishnavinod@gmail.com>
2024-04-18 22:25:24 +0530

    color change

--- flutter_application_2/lib/components/my_textfield.dart
+++ flutter_application_2/lib/components/my_textfield.dart
@@ -1,16 +1,51 @@
+// import 'package:flutter/material.dart';
+
+// class MyTextField extends StatelessWidget {
+//   final controller;
+//   final String hintText;
+//   final bool obscureText;
+//   const MyTextField(
+//       {super.key,
+//       required this.controller,
+//       required this.hintText,
+//       required this.obscureText});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 25),
+//       child: TextField(
+//         controller: controller,
+//         decoration: InputDecoration(
+//             enabledBorder: OutlineInputBorder(
+//                 borderSide: const BorderSide(color: Colors.white),
+//                 borderRadius: BorderRadius.circular(12)),
+//             focusedBorder: const OutlineInputBorder(
+//               borderSide: BorderSide(color: Color.fromARGB(106, 247, 0, 255)),
+//               borderRadius: BorderRadius.circular(12)
+//             ),
+//             fillColor: Colors.grey.shade200,
+//             filled: true,
+//             hintText: hintText,
+//             hintStyle: TextStyle(color: Colors.grey[500])),
+//         obscureText: obscureText,
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  final controller;
+  final TextEditingController controller;
   final String hintText;
-  final bool  obscureText;
+  final bool obscureText;
   const MyTextField({
-    super.key,
+    Key? key,
     required this.controller,
     required this.hintText,
-    required this.obscureText
-    }
-  );
+    required this.obscureText,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +54,20 @@ class MyTextField extends StatelessWidget {
       child: TextField(
         controller: controller,
         decoration: InputDecoration(
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            fillColor: Colors.grey.shade200,
-            filled: true,
-            hintText: hintText,
-            hintStyle: TextStyle(color: Colors.grey[500])
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Color(0x9A008A2A)),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0x9A008A2A)),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
         ),
-          obscureText: obscureText,
+        obscureText: obscureText,
       ),
     );
   }
