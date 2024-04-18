9df2286759b52c5ed91119136533c82bc0296248
TianKaiJr <user.gopikrishnavinod@gmail.com>
2024-04-18 20:15:08 +0530

    signout added

--- flutter_application_2/lib/app_pages/profile_page.dart
+++ flutter_application_2/lib/app_pages/profile_page.dart
@@ -1,17 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile Page",style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.black,
-      ),
       body: Center(
-        child: Text("Profile"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Signed in as : ${user.email!}'),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: const Color.fromARGB(129, 13, 5, 5),
+              child: const Text('Sign Out'),
+            )
+          ],
+        ),
       ),
     );
   }
