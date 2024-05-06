84aaf56da2979866672d70e2ea50d1386695aa58
TianKaiJr <user.gopikrishnavinod@gmail.com>
2024-05-06 21:18:18 +0530

    pop up of the lists in updates page implemented

--- flutter_application_2/lib/app_pages/updates_page.dart
+++ flutter_application_2/lib/app_pages/updates_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/components/showupdate_popup.dart';
 import 'package:flutter_application_2/services/firebase_storage_functions.dart';
 import 'package:flutter_application_2/services/firestore.dart';
 
@@ -111,96 +112,103 @@ class _UpdatesPageState extends State<UpdatesPage> {
                 String updateTextDetails = data['News Update Details'];
                 String imageURL = data['ImageURL'];
 
-                return Container(
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(
-                        192, 119, 119, 119), // Darker color for the list tile
-                    borderRadius: BorderRadius.circular(16.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        spreadRadius: 2,
-                        blurRadius: 4,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      updateTextHeading,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white, // Text color
-                      ),
-                    ),
-                    subtitle: Text(
-                      updateTextDetails,
-                      style: const TextStyle(
-                        color: Colors.white, // Text color
-                      ),
-                    ),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        PopupMenuButton(
-                          icon: const Icon(
-                            Icons.more_vert,
-                            color: Colors.white, // Icon color
-                          ),
-                          itemBuilder: (BuildContext context) =>
-                              <PopupMenuEntry>[
-                            PopupMenuItem(
-                              child: ListTile(
-                                leading: const Icon(
-                                  Icons.edit,
-                                  color: Colors.black, // Icon color
-                                ),
-                                title: const Text('Edit'),
-                                onTap: () {
-                                  Navigator.pop(context);
-                                  openNoteBox(docID: docID);
-                                },
-                              ),
-                            ),
-                            PopupMenuItem(
-                              child: ListTile(
-                                leading: const Icon(
-                                  Icons.delete,
-                                  color: Colors.black, // Icon color
-                                ),
-                                title: const Text('Delete'),
-                                onTap: () {
-                                  Navigator.pop(context);
-                                  fireStoreService.deleteNode(docID);
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                            width:
-                                8), // Add spacing between the PopupMenuButton and the image box
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                              10), // Adjust the radius as needed
-                          child: Container(
-                            width: 100, // Adjust the width as needed
-                            height: 50, // Adjust the height as needed
-                            color: Colors.grey, // Placeholder color
-                            child: imageURL.isNotEmpty
-                                ? Image.network(
-                                    imageURL,
-                                    fit: BoxFit.fill,
-                                  )
-                                : const Icon(
-                                    Icons.add_photo_alternate,
-                                    color: Colors.white,
-                                  ),
-                          ),
+                return GestureDetector(
+                  onTap: () {
+                    // POP UP of the list
+                    showCustomDialog(context, updateTextHeading,
+                        updateTextDetails, imageURL);
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 16.0),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(
+                          192, 119, 119, 119), // Darker color for the list tile
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: const Offset(0, 2),
                         ),
                       ],
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        updateTextHeading,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white, // Text color
+                        ),
+                      ),
+                      subtitle: Text(
+                        updateTextDetails,
+                        style: const TextStyle(
+                          color: Colors.white, // Text color
+                        ),
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          PopupMenuButton(
+                            icon: const Icon(
+                              Icons.more_vert,
+                              color: Colors.white, // Icon color
+                            ),
+                            itemBuilder: (BuildContext context) =>
+                                <PopupMenuEntry>[
+                              PopupMenuItem(
+                                child: ListTile(
+                                  leading: const Icon(
+                                    Icons.edit,
+                                    color: Colors.black, // Icon color
+                                  ),
+                                  title: const Text('Edit'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    openNoteBox(docID: docID);
+                                  },
+                                ),
+                              ),
+                              PopupMenuItem(
+                                child: ListTile(
+                                  leading: const Icon(
+                                    Icons.delete,
+                                    color: Colors.black, // Icon color
+                                  ),
+                                  title: const Text('Delete'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    fireStoreService.deleteNode(docID);
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                              width:
+                                  8), // Add spacing between the PopupMenuButton and the image box
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                10), // Adjust the radius as needed
+                            child: Container(
+                              width: 100, // Adjust the width as needed
+                              height: 50, // Adjust the height as needed
+                              color: Colors.grey, // Placeholder color
+                              child: imageURL.isNotEmpty
+                                  ? Image.network(
+                                      imageURL,
+                                      fit: BoxFit.fill,
+                                    )
+                                  : const Icon(
+                                      Icons.add_photo_alternate,
+                                      color: Colors.white,
+                                    ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
